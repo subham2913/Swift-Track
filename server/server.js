dae50f5a53b82d241e7a6aa2a17cf3567419a155
Subham Tiwari <tiwarist625940@gmail.com>
2026-04-11 00:11:26 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 app.use(cors({
   origin: "https://your-frontend.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
