ebd6a61d148d7169e15405d18bc68f0974f4d7b4
Subham Tiwari <tiwarist625940@gmail.com>
2026-04-10 22:56:10 +0530

    dep

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const API = process.env.REACT_APP_API_URL;
 export const axiosInstance = axios.create({
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
+  baseURL: API,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 require("dotenv").config();
+const cors = require("cors");
+
 const dbConfig = require("./config/dbConfig");
 
 // Middleware
 app.use(express.json());
+app.use(cors({
+  origin: "https://your-frontend.vercel.app",
+  credentials: true
+}));
 
 // Routes
 const usersRoute = require("./routes/usersRoute");
@@ -16,19 +21,13 @@ app.use("/api/users", usersRoute);
 app.use("/api/buses", busesRoute);
 app.use("/api/bookings", bookingsRoute);
 
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-
-// Serve static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
-
-
-// Set up the port
+// Port
 const port = process.env.PORT || 5000;
 
-// Start the server
-app.listen(port, () => console.log(`Node server listening on port ${port}!`));
+// Start server
+app.listen(port, () => console.log(`Server running on ${port}`));
